07e8161032235b056f9ae1fee66fa1ab0bd94320
birjolaxew <johanringmann@gmail.com>
2018-09-29 01:04:06 +0200

    Add support for specifying config in map

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // @ts-check
+const path = require("path");
 const transpiler = require("vmf-transpiler");
 const rXmasLights = require("./rules/christmas_lights");
 
@@ -9,11 +10,16 @@ const rXmasLights = require("./rules/christmas_lights");
  * @return {string} The processed VMF source
  */
 module.exports = function processVMF(vmfSource, config = { rules: {} }) {
-    config.rules = config.rules || {};
     const json = transpiler.parse(vmfSource, null);
 
-    // TODO: load config from map if present
+    // load config from map if it is specified
+    if (json.world && json.world.vmft_config) {
+        const configPath = path.resolve(process.cwd(), json.world.vmft_config);
+        config = require(configPath);
+    }
 
+    // then apply all our rules
+    config.rules = config.rules || {};
     rXmasLights(json, config.rules.christmas_lights);
 
     return transpiler.compile(json);
